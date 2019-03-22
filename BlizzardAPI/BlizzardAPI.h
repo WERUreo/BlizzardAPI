b8ab35ea10e5af4674404b184af9d6d35c80d2fe
Keli'i Martin <kelii.d.martin@gmail.com>
2019-03-22 12:58:43 -0400

    Initial Commit

---
+++ BlizzardAPI/BlizzardAPI.h
@@ -0,0 +1,19 @@
+//
+//  BlizzardAPI.h
+//  BlizzardAPI
+//
+//  Created by Kelii Martin on 3/22/19.
+//  Copyright © 2019 WERUreo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlizzardAPI.
+FOUNDATION_EXPORT double BlizzardAPIVersionNumber;
+
+//! Project version string for BlizzardAPI.
+FOUNDATION_EXPORT const unsigned char BlizzardAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlizzardAPI/PublicHeader.h>
+
+
